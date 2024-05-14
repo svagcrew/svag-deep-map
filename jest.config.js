7eab2009c180b7842709800b278311b709f5dea7
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-14 08:41:53 +0400

    Small fix

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 import svagJestConfigBase from 'svag-jest/configs/base.js'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import tsconfigData from './tsconfig.json' with { type: 'json' }
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   ...svagJestConfigBase,
+  ...(!!tsconfigData.compilerOptions?.paths && {
+    moduleNameMapper: pathsToModuleNameMapper(tsconfigData.compilerOptions.paths, { prefix: '<rootDir>/' }),
+  }),
 }
